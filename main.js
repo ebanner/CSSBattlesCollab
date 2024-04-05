1c3af25b231b65651d737c59e098946a8f6cb94c
Edward Banner <edward.banner@gmail.com>
2024-04-05 12:42:28 -0400

    Reorder websocket commands

--- main.js
+++ main.js
@@ -2,15 +2,16 @@ initializeWebSocketConnection()
 
 socket = io('http://127.0.0.1:5000');
 
+//
+// Receive messages
+//
+socket.on('server_response', function (response) {
+    console.log('Received response from server:', response);
+});
+
 document.addEventListener('mousemove', handleMouseMove);
 
 // Send a message to the server
 const message = 'Hello, server!';
 socket.send(message);
-
-
-// Receive messages
-socket.on('server_response', function (response) {
-    console.log('Received response from server:', response);
-});
 
