ee6a9ea83357a0c9e819a0f3f4daa221a6167786
Edward Banner <edward.banner@gmail.com>
2024-04-06 20:28:23 -0400

    Get slider sharing working
    V1 IS FUNCTIONAL

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
+socket = io('http://127.0.0.1:5000');
+
+
+//
+// TEXT FIELD STUFF
+//
+
 var TEXT_FIELD_LOCK = false;
 
-socket = io('http://127.0.0.1:5000');
 
 socket.on('text_changed', function (response) {
     console.log('Received response from server:', response);
@@ -9,11 +15,8 @@ socket.on('text_changed', function (response) {
     getTextFieldElement().innerHTML = response
 });
 
-//
-// Observe when the text field changes
-//
 
-var textFieldCallback = function(mutationsList, observer) {
+var textFieldChangedCallback = function(mutationsList, observer) {
     console.log('IN CALLBACK TEXT_FIELD_LOCK', TEXT_FIELD_LOCK);
     if (TEXT_FIELD_LOCK) {
         TEXT_FIELD_LOCK = false;
@@ -36,6 +39,49 @@ var textFieldCallback = function(mutationsList, observer) {
 //
 var textFieldElement = getTextFieldElement();
 var config = { characterData: true, subtree: true };
-var observer = new MutationObserver(textFieldCallback);
+var observer = new MutationObserver(textFieldChangedCallback);
 observer.observe(textFieldElement, config);
+
+
+//
+// SLIDER STUFF
+//
+
+var SLIDER_LOCK = false;
+
+socket.on('slider_changed', function (response) {
+    console.log('Received response from server:', response);
+    SLIDER_LOCK = true;
+    console.log('SET SLIDER_LOCK', SLIDER_LOCK)
+    var slider = JSON.parse(response);
+    setSlider(slider)
+});
+
+var sliderChangedCallback = function(mutationsList, observer) {
+    console.log('IN sliderChangedCallback SLIDER_LOCK', SLIDER_LOCK);
+    if (SLIDER_LOCK) {
+        SLIDER_LOCK = false;
+        return;
+    }
+
+    for (var mutation of mutationsList) {
+        if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
+            // Style attribute has changed, execute your event handler here
+            console.log('Style changed:', mutation);
+            // Call your event handler function here
+            slider = getSlider()
+            sliderSerialized = JSON.stringify(slider)
+            socket.send(sliderSerialized);
+        }
+    }
+};
+
+
+//
+// Attach an event handler to slider to broadcast changes
+//
+var sliderElement = getSliderElement();
+var config = { attributes: true };
+var observer = new MutationObserver(sliderChangedCallback);
+observer.observe(sliderElement, config);
 
